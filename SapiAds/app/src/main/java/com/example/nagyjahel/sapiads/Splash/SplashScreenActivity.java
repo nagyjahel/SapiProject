ad70d91bd8412167b5c4b365b5cf039b268b8260
palmarozalia.osztian <palmarozalia.osztian@gmail.com>
2018-11-24 19:17:36 +0200

    Dependencies
    Change implementation dependencies to 16.0.1

--- SapiAds/app/src/main/java/com/example/nagyjahel/sapiads/Splash/SplashScreenActivity.java
+++ SapiAds/app/src/main/java/com/example/nagyjahel/sapiads/Splash/SplashScreenActivity.java
@@ -1,8 +1,10 @@
 package com.example.nagyjahel.sapiads.Splash;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.nagyjahel.sapiads.Authentication.AuthenticationActivity;
 import com.example.nagyjahel.sapiads.R;
 
 public class SplashScreenActivity extends AppCompatActivity {
@@ -11,5 +13,8 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        Intent authenticationWindow = new Intent(this, AuthenticationActivity.class);
+        startActivity(authenticationWindow);
     }
+
 }
