f4be3f28b3a39cd9c40c04c57e95d384222be88e
nagyjahel <nagy.jahel@gmail.com>
2018-12-06 01:06:33 +0200

    Clear repository from unnecessary files

--- SapiProject/SapiAds/app/src/main/java/com/example/nagyjahel/sapiads/Main/Interfaces/OnAdvertisementManagingListener.java
+++
@@ -1,8 +0,0 @@
-package com.example.nagyjahel.sapiads.Main.Interfaces;
-
-public interface OnAdvertisementManagingListener {
-
-    void editAdvertisement();
-    void deleteAdvertisement();
-    void reportAdvertisement();
-}
