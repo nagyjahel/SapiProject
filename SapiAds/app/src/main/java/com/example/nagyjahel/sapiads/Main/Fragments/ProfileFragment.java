ad107c3907de3aa33306b8bf082d64f07638bb3e
palmarozalia.osztian <palmarozalia.osztian@gmail.com>
2018-12-06 15:15:36 +0200

    Profile Fragment - view current user details

--- SapiAds/app/src/main/java/com/example/nagyjahel/sapiads/Main/Fragments/ProfileFragment.java
+++ SapiAds/app/src/main/java/com/example/nagyjahel/sapiads/Main/Fragments/ProfileFragment.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,13 +12,20 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.example.nagyjahel.sapiads.Database.Models.Ad;
 import com.example.nagyjahel.sapiads.Database.Models.User;
 import com.example.nagyjahel.sapiads.R;
+import com.firebase.client.Firebase;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.FirebaseError;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ValueEventListener;
 import com.google.gson.Gson;
 
 import java.util.HashMap;
@@ -29,9 +37,12 @@ public class ProfileFragment extends Fragment {
     private EditText mLastNameValue;
     private EditText mPhoneNumber;
     private ImageView mProfilePicture;
+    private static final String TAG = "ProfileFragment";
 
+    private String phone;
     private Button mSaveButton;
 
+    private FirebaseAuth mAuth;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -40,6 +51,7 @@ public class ProfileFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
     }
 
 
@@ -47,14 +59,42 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        Log.d(TAG, "Profile fragment created");
+
+        final View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        initView(view);
 
         final FirebaseDatabase database = FirebaseDatabase.getInstance();
-        String key = Long.toString(System.currentTimeMillis());
-        final DatabaseReference ref = database.getReference("ads/" + key);
-        final FirebaseAuth user = FirebaseAuth.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Log.d(TAG, "Current user is not null");
+            phone = currentUser.getPhoneNumber();
 
+            //String name = currentUser.getDisplayName();
+            //String photoUrl = currentUser.getPhotoUrl().toString();
+            FirebaseDatabase.getInstance().getReference("users/" + phone).addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                        String firstName = (String)dataSnapshot.child("firstName").getValue();
+                        String lastName = (String)dataSnapshot.child("lastName").getValue();
+                        String photoUrl = (String)dataSnapshot.child("photoUrl").getValue();
+                        Log.d(TAG, firstName + " " + lastName + " " + photoUrl);
+                        mFirstNameValue.setText(firstName);
+                        mLastNameValue.setText(lastName);
+                        mPhoneNumber.setText(phone);
+                        Glide.with(view.getContext())
+                            .asBitmap()
+                            .load(photoUrl)
+                            .into(mProfilePicture);
+                }
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                }
+            });
+        }
 
-       /* mSaveButton.setOnClickListener(new View.OnClickListener() {
+       mSaveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (!mFirstNameValue.getText().toString().equals("") && !mLastNameValue.getText().toString().equals("") && !mPhoneNumber.getText().toString().equals("")) {
@@ -66,16 +106,20 @@ public class ProfileFragment extends Fragment {
                 }
 
             }
-        });*/
-        View view = inflater.inflate(R.layout.fragment_profile, container, false);
-        /*mSaveButton.findViewById(R.id.saveButton);
-        mFirstNameValue.findViewById(R.id.firstNameValue);
-        mLastNameValue.findViewById(R.id.lastNameValue);
-        mPhoneNumber.findViewById(R.id.phoneNumber);
+        });
 
-        mPhoneNumber.setText(user.getCurrentUser().getProviderId());
-        mFirstNameValue.setText(user.getCurrentUser().getDisplayName());
-        mLastNameValue.setText(user.getCurrentUser().getDisplayName());*/
+
         return view;
     }
+
+    private void initView(View view) {
+        mSaveButton = view.findViewById(R.id.saveButton);
+        mFirstNameValue = view.findViewById(R.id.firstNameValue);
+        mLastNameValue = view.findViewById(R.id.lastNameValue);
+        mPhoneNumber = view.findViewById(R.id.phoneNumber);
+        mProfilePicture = view.findViewById(R.id.profilePicture);
+    }
+
+
+
 }
